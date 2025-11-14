f6a05230fd7e94d456e4522e7e0dddc5df825cd5
OrangeBugatti <123387724+OrangeBugatti@users.noreply.github.com>
2025-11-14 09:30:55 -0500

    Create shared.lua

---
+++ shared.lua
@@ -0,0 +1,16 @@
+ENT.Type            = "anim"
+ENT.Base            = "base_gmodentity"
+ENT.PrintName       = "Quantum Money Printer"
+ENT.Author          = "GMOD Assistant"
+ENT.Spawnable       = true
+ENT.Category        = "DarkRP - Money Printers"
+
+ENT.PrintRate       = 7         -- seconds to generate money
+ENT.PrintAmount     = 95        -- dollars each cycle
+ENT.MaxHeat         = 100       -- overheat limit
+ENT.HeatIncrease    = 6         -- heat added per cycle
+ENT.CoolRate        = 2         -- heat lost per second
+
+-- Upgrade multipliers
+ENT.MultHeat        = 1
+ENT.MultMoney       = 1
